79929753f2237a67132d4917975989935c0bfe0d
santosh <sharma.santosh2121@gmail.com>
2024-06-28 20:58:01 +0530

    add pojo class

--- src/test/java/com/boldsignapi/pojos/ContactPojo.java
+++ src/test/java/com/boldsignapi/pojos/ContactPojo.java
@@ -3,9 +3,26 @@
  */
 package com.boldsignapi.pojos;
 
+import lombok.Builder;
+import lombok.Data;
+
 /**
  * 
  */
+@Builder
+@Data
 public class ContactPojo {
-
+	
+	private String email;
+	private String name;
+	private Phone phoneNumber;
+	private String jobTitle;
+	private String companyName;
+	
+	@Data
+	public static class Phone {
+		
+		private String countryCode;
+		private String number;
+	}
 }
