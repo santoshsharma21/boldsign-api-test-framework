289521d833a235ccee63725873d5cca8b90ade95
santosh <sharma.santosh2121@gmail.com>
2024-06-28 20:54:22 +0530

    add methods in api class

--- src/main/java/com/boldsignapi/api/ContactApi.java
+++ src/main/java/com/boldsignapi/api/ContactApi.java
@@ -3,9 +3,33 @@
  */
 package com.boldsignapi.api;
 
+import com.boldsignapi.restutils.HttpMethod;
+import com.boldsignapi.routes.Endpoints;
+
+import io.restassured.response.Response;
+
 /**
  * 
  */
 public class ContactApi {
-
+	
+	// fetch contact
+	public static Response retrieveContact(String id) {
+		return HttpMethod.get(Endpoints.GET_CONTACT, id);
+	}
+	
+	// create contacts
+	public static Response createContacts(Object payload) {
+		return HttpMethod.post(Endpoints.NEW_CONTACT, payload);
+	}
+	
+	// update contacts
+	public static Response updateContact(Object payload, String id) {
+		return HttpMethod.put(Endpoints.UPDATE_CONTACT, payload, id);
+	}
+	
+	// delete contact
+	public static Response deleteContact(String id) {
+		return HttpMethod.delete(Endpoints.DELETE_CONTACT, id);
+	}
 }
