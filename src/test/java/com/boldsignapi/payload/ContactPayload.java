0b22ec72c5785567c2cd03b130f52137e7591529
santosh <sharma.santosh2121@gmail.com>
2024-07-06 15:06:52 +0530

    add method to create request payload

--- src/test/java/com/boldsignapi/payload/ContactPayload.java
+++ src/test/java/com/boldsignapi/payload/ContactPayload.java
@@ -6,6 +6,8 @@ package com.boldsignapi.payload;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 import com.boldsignapi.pojos.ContactPojo;
 import com.boldsignapi.pojos.ContactPojo.Phone;
 import com.github.javafaker.Faker;
@@ -18,6 +20,11 @@ public class ContactPayload {
 	// init faker
 	private static Faker faker;
 
+	// method generates fake number
+	public static String getRandomPhoneNum() {
+		return "12345678" + RandomStringUtils.randomNumeric(2);
+	}
+
 	// new contact payload
 	public static List<ContactPojo> getNewContactPayload() {
 		// faker
@@ -28,8 +35,8 @@ public class ContactPayload {
 
 		for (int i = 0; i < 2; i++) {
 			Phone ph = new Phone();
-			ph.setCountryCode("+1");
-			ph.setNumber(faker.phoneNumber().cellPhone());
+			ph.setCountryCode("+91");
+			ph.setNumber(getRandomPhoneNum());
 
 			ContactPojo cpojo = ContactPojo.builder().email(faker.internet().emailAddress())
 					.name(faker.name().fullName()).phoneNumber(ph).jobTitle(faker.job().title())
@@ -43,10 +50,12 @@ public class ContactPayload {
 
 	// update paylod
 	public static ContactPojo getContactUpdatePayload() {
-
+		// faker
+		faker = new Faker();
+		
 		Phone ph = new Phone();
-		ph.setCountryCode("+1");
-		ph.setNumber(faker.phoneNumber().cellPhone());
+		ph.setCountryCode("+91");
+		ph.setNumber(getRandomPhoneNum());
 
 		return ContactPojo.builder().email(faker.internet().emailAddress()).name(faker.name().fullName())
 				.phoneNumber(ph).jobTitle(faker.job().title()).companyName(faker.company().name()).build();
