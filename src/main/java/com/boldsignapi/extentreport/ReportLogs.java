25a236c8e0da88900537b0abe89fb05e07fd148f
santosh <sharma.santosh2121@gmail.com>
2024-07-06 15:08:05 +0530

    update framework

---
+++ src/main/java/com/boldsignapi/extentreport/ReportLogs.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.boldsignapi.extentreport;
+
+import com.aventstack.extentreports.markuputils.CodeLanguage;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+
+/**
+ * 
+ */
+public class ReportLogs {
+	
+	// method logs string
+	public static void logText(String text) {
+		ReportManager.extentTest.info(MarkupHelper.createLabel(text, ExtentColor.BLUE));
+	}
+	
+	// method adds json into report
+	public static void logJson(String json) {
+		ReportManager.extentTest.info(MarkupHelper.createCodeBlock(json, CodeLanguage.JSON));
+	}
+}
